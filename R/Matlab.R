b96459bafe08c829928b1e3e85bc7e652d157773
Henrik Bengtsson <hb@aroma-project.org>
2013-07-11 18:12:27 +0000

    Update documentation with more modern MATLAB versions.
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/r-dots/pkg/R.matlab@1483 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/Matlab.R
+++ R/Matlab.R
@@ -122,17 +122,18 @@
 #
 # \section{Confirmed MATLAB versions}{
 #   This package has been confirmed to work \emph{successfully} out of
-#   the box together with:
+#   the box together with the following MATLAB versions:
 #   MATLAB v6.1.0.450 (R12.1),
 #   MATLAB v6.5.0.180913a (R13),
 #   MATLAB v7.0.0.19901 (R14),
 #   MATLAB v7.0.1.24704 (R14SP1),
 #   MATLAB v7.0.4.365 (R14SP2),
 #   MATLAB v7.2.0.232 (R2006a),
-#   MATLAB 7.4.0 (R2007a),
-#   MATLAB 7.7.0.471 (R2008b),
-#   MATLAB version 7.10.0.499 (R2010a), and
-#   MATLAB version 7.11.0.584 (R2010b).
+#   MATLAB v7.4.0 (R2007a),
+#   MATLAB v7.7.0.471 (R2008b),
+#   MATLAB v7.10.0.499 (R2010a),
+#   MATLAB v7.11.0.584 (R2010b), and
+#   MATLAB v7.14.0.739 (R2012a).
 #   If you successfully use a different/higher MATLAB version,
 #   please tell us, so we can share it here.
 #
