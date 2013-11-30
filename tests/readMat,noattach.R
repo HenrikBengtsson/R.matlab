d4e944af2af164160fbfc9bad539c12eae00e57c
Henrik Bengtsson <hb@aroma-project.org>
2013-11-30 21:15:12 +0000

    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/r-dots/pkg/R.matlab@1859 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- tests/readMat,noattach.R
+++ tests/readMat,noattach.R
@@ -1,4 +1,8 @@
 path <- system.file("mat-files", package="R.matlab")
-bs <- R.matlab::readMat(file.path(path, "unsignedByte.mat"))
-if (!identical(as.vector(bs$A), as.double(126:255)))
+data <- R.matlab::readMat(file.path(path, "unsignedByte.mat"))
+if (!identical(as.vector(data$A), as.double(126:255)))
   stop("Error reading unsigned bytes saved by MATLAB.")
+
+# Compressed data blocks - display raw bytes
+data <- R.matlab::readMat(file.path(path, "NestedMiMATRIX,problem4,v5,compressed.mat"), verbose=-120)
+
